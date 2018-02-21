5a91bfe1e60438c5de48d0883dc8241c20499391
Chris Bremmer <c_bremmer@outlook.com>
2018-02-21 12:35:32 -0800

    added 7kyu_regexBasics

---
+++ 7kyu_regexBasics-isItAVowel.js
@@ -0,0 +1,6 @@
+// Implement String#vowel? (in Java StringUtils.isVowel(String)), which should
+// return true if given object is a vowel, false otherwise.
+
+String.prototype.vowel = function() {
+  return /^[aeiou]$/i.test(this);
+};
