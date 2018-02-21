e8ec4456b9c8ddf570f12889964077faf129d756
Chris Bremmer <c_bremmer@outlook.com>
2018-02-21 12:54:58 -0800

    added 7kyu_printerErrors

---
+++ 7kyu_printerErrors.js
@@ -0,0 +1,58 @@
+// In a factory a printer prints labels for boxes. For one kind of boxes the
+// printer has to use colors which, for the sake of simplicity, are named with
+// letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example
+// a "good" control string would be aaabbbbhaijjjm meaning that the printer used
+// three times color a, four times color b, one time color h then one time color
+// a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a
+// "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
+
+// You have to write a function printer_error which given a string will output
+// the error rate of the printer as a string representing a rational whose
+// numerator is the number of errors and the denominator the length of the
+// control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters
+// from ato z.
+
+// #Examples:
+
+// s="aaabbbbhaijjjm" error_printer(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm" error_printer(s) => "8/22"
+
+function printerError(s) {
+  const totalLetters = s.length;
+
+  var incorrectLetters = [
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  var totalIncorrect = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let index = s[i];
+    for (let j = 0; j < incorrectLetters.length; j++) {
+      if (index == incorrectLetters[j]) {
+        totalIncorrect += 1;
+      }
+    }
+  }
+
+  return totalIncorrect + "/" + totalLetters;
+}
